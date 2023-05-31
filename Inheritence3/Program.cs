907894d26b53831c0822148c6a4f91595dc41b9f
beverley-dick <beverleydick11@gmail.com>
2023-05-31 09:43:25 +0300

    Add project files.

---
+++ Inheritence3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Inheritence3
+{
+    class Animal
+    {
+        public virtual void eat()
+        {
+            Console.WriteLine("I eat food");
+        } 
+        //derived class of Animal
+        class Dog : Animal
+        {
+            //overriding method from Animal
+            public override void eat()
+            {
+                Console.WriteLine("I eat Dog food");
+            }
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //object of derived class
+                Dog labrador = new Dog();
+                labrador.eat();
+
+                Console.ReadKey();
+            }
+        }
+    }
+}
